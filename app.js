4d651c0f1ce000541bbcfd6c09faccf13996fbd7
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-10-07 08:47:45 +0700

    makes up an HTTP request

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const request = require('request')
 
 request({
-    url: "https://jsonplaceholder.typicode.com/todos/14",
+    url: "https://jsonplaceholder.typicode.com/todos/11",
+    // url: "https://maps. googleapis.com/maps/api/geocode/json?address=Gg.%20Gotong%20Royong%20No.4%20Cilendek%20Bar.%20Bogor%20Bar.%20Kota%20Bogor%20Jawa%20Barat%2016112",
     json : true
 },(error, response, body)=> {
-    console.log(JSON.stringify(body, undefined, 5))
+    // console.log(JSON.stringify(body, undefined, 5))
+
+    console.log(`Title : ${body.title} `)
 } )
