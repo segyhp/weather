8d9c7941ba63064ca608f22af48ec8ba9815b8bd
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-10-07 16:12:08 +0700

    encoding user input

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-const request = require('request')
+const request = require('request');
+const yargs = require('yargs');
+
+
+const argv = yargs.options({
+//     a : {
+//         demand: true,
+//         alias : 'address',
+//         describe : 'Address to fetch data for',
+//         string: true 
+//     }
+// })
+    id_num : {
+        demand: true,
+        alias : 'id',
+        describe : 'Id to fetch data for',
+    }
+})
+.help()
+.alias('help', 'h')
+.argv; 
+
+console.log(argv)
+
+// var encodedAddress = encodeURIComponent(argv.address)
+var encodedAddress = encodeURIComponent(argv.id_num)
+
+
+
+
+
 
 request({
-    url: "https://jsonplaceholder.typicode.com/todos/11",
+    url: `https://jsonplaceholder.typicode.com/todos/${encodedAddress}`,
     // url: "https://maps. googleapis.com/maps/api/geocode/json?address=Gg.%20Gotong%20Royong%20No.4%20Cilendek%20Bar.%20Bogor%20Bar.%20Kota%20Bogor%20Jawa%20Barat%2016112",
     json : true
 },(error, response, body)=> {
-    // console.log(JSON.stringify(body, undefined, 5))
+    console.log(JSON.stringify(body, undefined, 5))
 
-    console.log(`Title : ${body.title} `)
+    // console.log(`Title : ${body.title} `)
 } )
