3864dc9d966a110bc1d5fd51cfbb5298e7e8a52c
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-10-11 16:29:56 +0700

    abstracting callback

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const request = require('request');
 const yargs = require('yargs');
-
-const API_KEY = 'ba49b8159b84217d7b203c0007372b74'
+const geocode = require('./geocode/geocode')
 const argv = yargs.options({
-    a : {
+    l : {
         demand: true,
-        alias : 'city',
+        alias : 'location',
         describe : 'City name to fetch data for',
         string: true 
     }
@@ -15,34 +13,14 @@ const argv = yargs.options({
 .alias('help', 'h')
 .argv; 
 
-// console.log(argv)
-
-var encodedCity = encodeURIComponent(argv.city)
-
-
-
-
-
-
-request({
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${encodedCity}&appid=${API_KEY}`,
-    // url: "https://maps. googleapis.com/maps/api/geocode/json?address=Gg.%20Gotong%20Royong%20No.4%20Cilendek%20Bar.%20Bogor%20Bar.%20Kota%20Bogor%20Jawa%20Barat%2016112",
-    json : true
-},(error, response, body)=> {
-    // console.log(JSON.stringify(body, undefined, 5))
-    if(error){
-        console.log("Unable connect to API services")
-    } else if(body.cod === "404"){
-        console.log("Error : " ,body.message)
+geocode.geoCodeAddress(argv.location, (errorMessage, results) => {
+    if(errorMessage){
+        console.log(errorMessage)
     }
-     else if(body.cod === 200){
-        console.log(`Location : ${body.name} `);
-        console.log(`Weather : ${body.weather[0].main} - ${body.weather[0].description}  `);
-        console.log(`Longitude : ${body.coord.lon} `);
-        console.log(`Latitude : ${body.coord.lat} `);
+    else {
+        console.log(JSON.stringify(results, undefined, 2))
     }
-    // else {
-   
-    // }
-  
-} )
+})
+
+
+// geocode.geoCodeAddress(argv.location)
---
+++ geocode/geocode.js
@@ -0,0 +1,44 @@
+const request = require('request');
+const API_KEY = 'ba49b8159b84217d7b203c0007372b74'
+
+
+var geoCodeAddress = (location, callback)=> {
+    var encodedLocation = encodeURIComponent(location)
+    request({
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${encodedLocation}&appid=${API_KEY}`,
+    // url: "https://maps. googleapis.com/maps/api/geocode/json?address=Gg.%20Gotong%20Royong%20No.4%20Cilendek%20Bar.%20Bogor%20Bar.%20Kota%20Bogor%20Jawa%20Barat%2016112",
+    json: true
+}, (error, response, body) => {
+    // console.log(JSON.stringify(body, undefined, 5))
+    if (error) {
+        callback("Unable connect to API services.");
+        // console.log("Unable connect to API services")
+    } else if (body.cod === "404") {
+        // console.log("Error : ", body.message)
+        callback("Error : ", body.message )
+    } else if (body.cod === 200) {
+        callback(undefined, {
+            location: body.name,
+            weather: body.weather[0].main+ ' - '+ body.weather[0].description,
+            longitde : body.coord.lon,
+            latitude : body.coord.lat
+        })
+        // console.log(`Location : ${body.name} `);  
+        // console.log(`Weather : ${body.weather[0].main} - ${body.weather[0].description}  `);
+        // console.log(`Longitude : ${body.coord.lon} `);
+        // console.log(`Latitude : ${body.coord.lat} `);
+    }
+    // else { 
+
+    // }
+
+})
+}
+
+
+
+// module.exports = {
+//     geoCodeAddress
+// }
+
+module.exports.geoCodeAddress = geoCodeAddress;
